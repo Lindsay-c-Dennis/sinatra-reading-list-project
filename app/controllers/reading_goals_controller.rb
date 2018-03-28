2a4e25aeb437dade7b4429a0ae4117a6dd948295
Lindsay <lindsay.c.dennis@gmail.com>
2018-03-28 16:03:30 -0500

    build edit function for reading goals

--- app/controllers/reading_goals_controller.rb
+++ app/controllers/reading_goals_controller.rb
@@ -6,7 +6,24 @@ class ReadingGoalsController < ApplicationController
     else
     	redirect to '/'
     end
+  end 
+
+  get '/reading_goals/:id/edit' do 
+    @goal = ReadingGoal.find_by_id(params[:id])
+    if logged_in? && current_user.id == @goal.user_id
+      erb :'/reading_goals/edit'
+    else
+      redirect to '/'
+    end
+  end 
+
+  patch '/reading_goals/:id/edit' do 
+    @goal = ReadingGoal.find_by_id(params[:id])
+    @goal.update(content: params[:content])
+    redirect to '/users/:id'
   end  
+
+       
 
   post '/reading_goals' do
      @goal = ReadingGoal.create(content: params[:content])
